3e22cffe652192e3fbddfbaaae55ceb4c33764f8
Xadoy <Adoy@Macbook-2.local>
2016-06-16 12:51:13 +0800

    fourthview
    fourth view

---
+++ MyCNbeta/MyCNbeta/FourthViewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FourthViewTableViewController.h
+//  MyCNbeta
+//
+//  Created by Adoy on 6/16/16.
+//  Copyright © 2016 Adoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FourthViewTableViewController : UITableViewController
+
+@end
---
+++ MyCNbeta/MyCNbeta/FourthViewTableViewController.m
@@ -0,0 +1,98 @@
+//
+//  FourthViewTableViewController.m
+//  MyCNbeta
+//
+//  Created by Adoy on 6/16/16.
+//  Copyright © 2016 Adoy. All rights reserved.
+//
+
+#import "FourthViewTableViewController.h"
+
+@interface FourthViewTableViewController ()
+
+@end
+
+@implementation FourthViewTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    UILabel *title = [[UILabel alloc]init];
+    title.text = @"设置";
+    title.textColor = [UIColor whiteColor];
+    title.textAlignment=NSTextAlignmentCenter;
+    title.frame = CGRectMake(0, 0, 122,38);
+    self.navigationController.topViewController.navigationItem.titleView = title;
+    
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
